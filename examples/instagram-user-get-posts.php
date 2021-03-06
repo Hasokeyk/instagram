7c8d7b7b9dfa6daccbc9bf8e1e3e5a651d1fe819
Hasan Yüksektepe <hasan.yuksektepe@ogr.iu.edu.tr>
2021-03-06 15:50:38 +0300

    1.0.4
    - Yeni örnekler eklendi

---
+++ examples/instagram-user-get-posts.php
@@ -0,0 +1,21 @@
+<?php
+    
+    use instagram\instagram;
+    
+    require "vendor/autoload.php";
+    //require "src/instagram.php";
+    
+    $username = 'username';
+    $password = 'password';
+    
+    $instagram = new instagram($username,$password);
+    
+    $login = $instagram->login->login_control();
+    if($login){
+    
+        $user_posts = $instagram->user->get_user_posts();
+        print_r($user_posts);
+        
+    }else{
+        echo 'Login Fail';
+    }
